aea9a88e04ece3dea52c5f27a1062eaf6bf735d9
Quentin Roy-Foster <quentinrf@gmail.com>
2024-10-02 21:05:38 -0400

    remove main func

--- main.go
+++ main.go
@@ -2,18 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: my-cli-tool <name>")
-		return
-	}
-
-	name := os.Args[1]
-	fmt.Printf("Hello, %s!\n", name)
-}
 
 func Greet(name string) {
 	fmt.Printf("Hello, %s from my-cli-tool!\n", name)
